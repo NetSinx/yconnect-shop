609116e3aff53b87649639fe66f32e95755aa8e6
NetSinx <yasin03ckm@gmail.com>
2024-09-07 08:37:48 +0700

    add package in go.mod

--- server/order/go.mod
+++ server/order/go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
@@ -32,5 +33,6 @@ require (
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- server/order/routes/routes.go
+++ server/order/routes/routes.go
@@ -1,13 +1,13 @@
 package routes
 
 import (
-	// "net/http"
+	"net/http"
 	"github.com/NetSinx/yconnect-shop/server/order/config"
 	"github.com/NetSinx/yconnect-shop/server/order/controller"
 	"github.com/NetSinx/yconnect-shop/server/order/repository"
 	"github.com/NetSinx/yconnect-shop/server/order/service"
 	"github.com/labstack/echo/v4"
-	// "github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func RoutesAPI() *echo.Echo {
@@ -16,15 +16,15 @@ func RoutesAPI() *echo.Echo {
 	orderController := controller.OrderContrllr(orderService)
 
 	router := echo.New()
-	// router.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
-	// 	TokenLookup: "header:xsrf",
-	// 	CookieName: "xsrf",
-	// 	CookiePath: "/",
-	// 	CookieMaxAge: 60,
-	// 	CookieHTTPOnly: true,
-	// 	CookieSameSite: http.SameSiteStrictMode,
-	// 	CookieSecure: true,
-	// }))
+	router.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		TokenLookup: "header:xsrf",
+		CookieName: "xsrf",
+		CookiePath: "/",
+		CookieMaxAge: 60,
+		CookieHTTPOnly: true,
+		CookieSameSite: http.SameSiteStrictMode,
+		CookieSecure: true,
+	}))
 	router.GET("/order/:username", orderController.ListOrder)
 	router.POST("/order", orderController.AddOrder)
 	router.DELETE("/order/:username/:id", orderController.DeleteOrder)
