9cecb5ad2c7d724d6ad16013df6f3f272a73e67a
NetSinx <yasin03ckm@gmail.com>
2025-01-21 13:27:26 +0700

    adding key auth middleware

--- server/category/go.mod
+++ server/category/go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
@@ -31,5 +32,6 @@ require (
 	golang.org/x/net v0.15.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- server/category/routes/routes.go
+++ server/category/routes/routes.go
@@ -6,6 +6,7 @@ import (
 	"github.com/NetSinx/yconnect-shop/server/category/repository"
 	"github.com/NetSinx/yconnect-shop/server/category/service"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func ApiRoutes() *echo.Echo {
@@ -14,6 +15,12 @@ func ApiRoutes() *echo.Echo {
 	categoryController := controller.CategoryController(categoryService)
 
 	router := echo.New()
+	router.Use(middleware.KeyAuthWithConfig(middleware.KeyAuthConfig{
+		KeyLookup: "header:api-token",
+		Validator: func(auth string, c echo.Context) (bool, error) {
+			return auth == "dfkgjdgj#753846873248358645*&#%^*$54%hgdf", nil
+		},
+	}))
 	router.GET("/category", categoryController.ListCategory)
 	router.GET("/category/:id", categoryController.GetCategory)
 	router.POST("/category", categoryController.CreateCategory)
