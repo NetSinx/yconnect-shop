9e99d8b4ec82ce53a0aa388a2c79f5003fd70b75
NetSinx <yasin03ckm@gmail.com>
2025-08-20 09:15:42 +0700

    adding helpers test

--- server/product/test/helpers_test.go
+++ server/product/test/helpers_test.go
@@ -1,12 +1,35 @@
 package test
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"strings"
 	"testing"
 )
 
+func TestGenerateSlugByName(t *testing.T) {
+	name := "Ayam Bakar Pedas Manis"
+
+	trimmed := strings.TrimSpace(name)
+	words := strings.Fields(trimmed)
+	name = strings.Join(words, " ")
+	name = strings.ToLower(name)
+	name = strings.ReplaceAll(name, " ", "-")
+
+	b := make([]byte, 8)
+	if _, err := rand.Read(b); err != nil {
+		t.Errorf("Error: %v", err)
+	}
+
+	uid := base64.URLEncoding.EncodeToString(b)
+	slug := name + "-" + uid
+
+	t.Logf("Result: %v", slug)
+}
+
 func TestReplaceProductSlug(t *testing.T) {
 	nama := "Ayam Bakar Pedas Manis"
+	
 	slug := "ayam-bakar-ergjklgr34gg=="
 	expected := "ayam-bakar-pedas-manis-ergjklgr34gg=="
 	splitSlug := strings.Split(slug, "-")
