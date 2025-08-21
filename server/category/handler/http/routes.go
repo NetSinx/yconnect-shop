cf9bb6aef48f14fde065cacdcadb0cd73d0d4375
NetSinx <yasin03ckm@gmail.com>
2025-08-21 10:00:48 +0700

    update category service

--- server/category/handler/http/routes.go
+++ server/category/handler/http/routes.go
@@ -1,10 +1,8 @@
 package http
 
 import (
-	"net/http"
-	"github.com/NetSinx/yconnect-shop/server/category/handler/dto"
+	"github.com/NetSinx/yconnect-shop/server/authentication/middleware"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 func ApiRoutes(e *echo.Echo, categoryHandler categoryHandler) {
@@ -14,19 +12,7 @@ func ApiRoutes(e *echo.Echo, categoryHandler categoryHandler) {
 	apiGroup.GET("/category/slug/:slug", categoryHandler.GetCategoryBySlug)
 
 	adminGroup := apiGroup.Group("/admin")
-	adminGroup.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
-		TokenLookup: "cookie:csrf_token",
-		CookieName: "csrf_token",
-		CookiePath: "/",
-		CookieHTTPOnly: true,
-		CookieMaxAge: 30,
-		CookieSecure: true,
-		ErrorHandler: func(err error, c echo.Context) error {
-			return echo.NewHTTPError(http.StatusBadRequest, dto.MessageResp{
-				Message: "csrf token not available",
-			})
-		},
-	}))
+	adminGroup.Use(middleware.CSRFMiddleware)
 	adminGroup.POST("/category", categoryHandler.CreateCategory)
 	adminGroup.PUT("/category/:slug", categoryHandler.UpdateCategory)
 	adminGroup.DELETE("/category/:slug", categoryHandler.DeleteCategory)
