481733e2c428fdff1ba65e0e6dd390cf99541a7b
NetSinx <yasin03ckm@gmail.com>
2025-09-02 09:22:00 +0700

    update auth service

--- server/authentication/internal/usecase/authentication_usecase.go
+++ server/authentication/internal/usecase/authentication_usecase.go
@@ -1,126 +1,26 @@
 package usecase
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"github.com/NetSinx/yconnect-shop/server/user/model/entity"
+	"github.com/NetSinx/yconnect-shop/server/authentication/internal/model"
 	"github.com/go-playground/validator/v10"
-	"github.com/golang-jwt/jwt/v5"
-	"golang.org/x/crypto/bcrypt"
+	"github.com/sirupsen/logrus"
 )
 
-type authService struct {
-	authServ  AuthService	
+type AuthUseCase struct {
+	Log *logrus.Logger
+	Validator *validator.Validate
 }
 
-func AuthServ() *authService {
-	var authServ authService
-	return &authService{
-		authServ: authServ.authServ,
+func NewAuthUseCase(log *logrus.Logger, validator *validator.Validate) *AuthUseCase {
+	return &AuthUseCase{
+		Log: log,
+		Validator: validator,
 	}
 }
 
-func (as *authService) LoginUser(userLogin dto.UserLogin) (string, string, error) {
-	if err := validator.New().Struct(userLogin); err != nil {
-		respUser, err := http.Get(fmt.Sprintf("http://localhost:8082/user?username=%s", userLogin.UsernameorEmail))
-		if err != nil {
-			return "", "", err
-		} else if respUser.StatusCode == 401 {
-			return "", "", fmt.Errorf("email atau password salah")
-		} else if respUser.StatusCode == 400 {
-			var respBadRequest dto.MessageResp
-			if err := json.NewDecoder(respUser.Body).Decode(&respBadRequest); err != nil {
-				return "", "", err
-			}
-			return "", "", fmt.Errorf("%v", respBadRequest.Message)
-		}
-
-		var user entity.User
-		if err := json.NewDecoder(respUser.Body).Decode(&user); err != nil {
-			return "", "", fmt.Errorf("failed to decode response")
-		}
-
-		err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userLogin.Password))
-		if err != nil {
-			return "", "", fmt.Errorf("email atau password salah")
-		}
-
-		accessToken := helpers.GenerateAccessToken(user.Username, user.Role)
-		if user.Role == "admin" {
-			signKey := []byte(helpers.AdminJwtKey)
-			_, err := jwt.ParseWithClaims(accessToken, &helpers.CustomClaims{}, func(t *jwt.Token) (any, error) {
-				return signKey, nil
-			})
-			if err != nil {
-				return "", "", fmt.Errorf("failed to parse token with claims")
-			}
-			refreshToken := helpers.GenerateRefreshToken(user.Username, user.Role)
-
-			return accessToken, refreshToken, nil
-		}
-
-		signKey := []byte(helpers.CustomerJwtKey)
-		_, err = jwt.ParseWithClaims(accessToken, &helpers.CustomClaims{}, func(t *jwt.Token) (any, error) {
-			return signKey, nil
-		})
-		if err != nil {
-			return "", "", fmt.Errorf("failed to parse token with claims")
-		}
-
-		refreshToken := helpers.GenerateRefreshToken(user.Username, user.Role)
-
-		return accessToken, refreshToken, nil
-	}
-
-	respUser, err := http.Get(fmt.Sprintf("http://localhost:8082/user?email=%s", userLogin.UsernameorEmail))
-	if err != nil {
-		return "", "", err
-	} else if respUser.StatusCode == 401 {
-		return "", "", fmt.Errorf("email atau password salah")
-	} else if respUser.StatusCode == 400 {
-		var respBadRequest dto.MessageResp
-		if err := json.NewDecoder(respUser.Body).Decode(&respBadRequest); err != nil {
-			return "", "", err
-		}
-		return "", "", fmt.Errorf("%s", respBadRequest.Message)
-	}
-
-	var user entity.User
-	if err := json.NewDecoder(respUser.Body).Decode(&user); err != nil {
-		return "", "", fmt.Errorf("failed to decode response")
-	}
-
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userLogin.Password))
-	if err != nil {
-		return "", "", fmt.Errorf("email atau password salah")
-	}
-
-	if user.Role == "admin" {
-		accessToken := helpers.GenerateAccessToken(user.Username, user.Role)
-		signKey := []byte(helpers.AdminJwtKey)
-		_, err := jwt.ParseWithClaims(accessToken, &helpers.CustomClaims{}, func(t *jwt.Token) (any, error) {
-			return signKey, nil
-		})
-		if err != nil {
-			return "", "", fmt.Errorf("failed to parse token with claims")
-		}
-	
-		refreshToken := helpers.GenerateRefreshToken(user.Username, user.Role)
-	
-		return accessToken, refreshToken, nil
-	} else {
-		accessToken := helpers.GenerateAccessToken(user.Username, user.Role)
-		signKey := []byte(helpers.CustomerJwtKey)
-		_, err := jwt.ParseWithClaims(accessToken, &helpers.CustomClaims{}, func(t *jwt.Token) (any, error) {
-			return signKey, nil
-		})
-		if err != nil {
-			return "", "", fmt.Errorf("failed to parse token with claims")
-		}
-	
-		refreshToken := helpers.GenerateRefreshToken(user.Username, user.Role)
-	
-		return accessToken, refreshToken, nil
+func (a *AuthUseCase) LoginUser(loginRequest *model.LoginRequest) string {
+	if err := a.Validator.Struct(loginRequest); err != nil {
+		a.Log.WithError(err).Error("error validating request body")
+		return ""
 	}
 }
