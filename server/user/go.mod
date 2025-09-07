f86c1a09c7a85f73b977b45824ed98354359ee9b
NetSinx <yasin03ckm@gmail.com>
2025-09-07 21:51:26 +0700

    adding subscriber in user service

--- server/user/go.mod
+++ server/user/go.mod
@@ -1,18 +1,18 @@
 module github.com/NetSinx/yconnect-shop/server/user
 
-go 1.21.0
+go 1.23.0
 
 require (
 	github.com/NetSinx/yconnect-shop/server/cart v0.0.0-20250125153909-bde6dbfc6fd1
 	github.com/NetSinx/yconnect-shop/server/order v0.0.0-20240813135508-69e6d9adca22
-	github.com/go-playground/validator/v10 v10.22.0
+	github.com/go-playground/validator/v10 v10.27.0
 	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo-jwt/v4 v4.3.0
-	github.com/labstack/echo/v4 v4.13.0
+	github.com/labstack/echo/v4 v4.13.4
 	github.com/rabbitmq/amqp091-go v1.10.0
 	github.com/redis/go-redis/v9 v9.6.1
-	golang.org/x/crypto v0.32.0
+	golang.org/x/crypto v0.38.0
 	gorm.io/driver/mysql v1.5.7
 	gorm.io/gorm v1.25.11
 )
@@ -37,7 +37,7 @@ require (
 	github.com/NetSinx/yconnect-shop/server/product v0.0.0-20240619004129-76d3c85416f7 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/gabriel-vasile/mimetype v1.4.5 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
@@ -45,14 +45,14 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/sirupsen/logrus v1.9.3
 	github.com/spf13/viper v1.20.1
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/net v0.33.0 // indirect
-	golang.org/x/sys v0.29.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
-	golang.org/x/time v0.8.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+	golang.org/x/time v0.11.0 // indirect
 )
--- server/user/internal/model/user_model.go
+++ server/user/internal/model/user_model.go
@@ -1,5 +1,18 @@
 package model
 
+type RegisterRequest struct {
+	NamaLengkap        string `json:"nama_lengkap" validate:"required,max=100"`
+	Username           string `json:"username" validate:"required,max=50"`
+	Email              string `json:"email" validate:"required,max=100,email"`
+	NoHP               string `json:"no_hp" validate:"required,max=12"`
+	Password           string `json:"password" validate:"passwd,required,min=5"`
+	KonfirmasiPassword string `json:"konfirmasi_password" validate:"eqfield=password"`
+}
+
+type DataResponse struct {
+	Data *UserResponse `json:"data"`
+}
+
 type UpdateAlamatRequest struct {
 	NamaJalan string `json:"nama_jalan" validate:"required,max=100"`
 	RT        int    `json:"rt" validate:"required"`
@@ -11,11 +24,13 @@ type UpdateAlamatRequest struct {
 }
 
 type UpdateUserRequest struct {
-	NamaLengkap string              `json:"nama_lengkap" validate:"required,max=100"`
-	Username    string              `json:"username" validate:"required,max=50"`
-	Email       string              `json:"email" validate:"required,max=100,email"`
-	Alamat      UpdateAlamatRequest `json:"alamat"`
-	NoHP        string              `json:"no_hp" validate:"required,max=12"`
+	NamaLengkap        string               `json:"nama_lengkap" validate:"required,max=100"`
+	Username           string               `json:"username" validate:"required,max=50"`
+	Email              string               `json:"email" validate:"required,max=100,email"`
+	Alamat             *UpdateAlamatRequest `json:"alamat"`
+	NoHP               string               `json:"no_hp" validate:"required,max=12"`
+	Password           string               `json:"password" validate:"passwd,required,min=5"`
+	KonfirmasiPassword string               `json:"konfirmasi_password" validate:"eqfield=password"`
 }
 
 type DeleteUserRequest struct {
@@ -38,12 +53,12 @@ type AlamatResponse struct {
 }
 
 type UserResponse struct {
-	ID          uint            `json:"id"`
-	NamaLengkap string          `json:"nama_lengkap"`
-	Username    string          `json:"username"`
-	Email       string          `json:"email"`
-	Alamat      *AlamatResponse `json:"alamat"`
-	NoHP        string          `json:"no_hp"`
+	ID                 uint            `json:"id"`
+	NamaLengkap        string          `json:"nama_lengkap"`
+	Username           string          `json:"username"`
+	Email              string          `json:"email"`
+	Alamat             *AlamatResponse `json:"alamat"`
+	NoHP               string          `json:"no_hp"`
 }
 
 type MessageResp struct {
