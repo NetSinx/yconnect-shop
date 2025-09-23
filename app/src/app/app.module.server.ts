5ab8489d46fde9990bec891a91bf582790e499c2
NetSinx <yasin03ckm@gmail.com>
2025-09-23 21:07:03 +0700

    adding ssr

--- app/src/app/app.module.server.ts
+++ app/src/app/app.module.server.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
-import { provideServerRendering, withRoutes } from '@angular/ssr';
+import { provideServerRendering, withRoutes, withAppShell } from '@angular/ssr';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 import { serverRoutes } from './app.routes.server';
+import { AppShellComponent } from './app-shell/app-shell.component';
 
 @NgModule({
   imports: [AppModule],
-  providers: [provideServerRendering(withRoutes(serverRoutes))],
+  providers: [provideServerRendering(withRoutes(serverRoutes), withAppShell(AppShellComponent))],
   bootstrap: [AppComponent],
+  declarations: [
+    AppShellComponent
+  ],
 })
 export class AppServerModule {}
