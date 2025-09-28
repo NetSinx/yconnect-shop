c3c4c838777aff1169ac7bcca4905f370b2e8899
NetSinx <yasin03ckm@gmail.com>
2025-09-28 11:04:22 +0700

    update interceptor in angular

--- app/src/app/app.module.server.ts
+++ app/src/app/app.module.server.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
-import { provideServerRendering, withRoutes, withAppShell } from '@angular/ssr';
+import { provideServerRendering, withRoutes } from '@angular/ssr';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
 import { serverRoutes } from './app.routes.server';
-import { AppShellComponent } from './app-shell/app-shell.component';
 
 @NgModule({
   imports: [AppModule],
-  providers: [provideServerRendering(withRoutes(serverRoutes), withAppShell(AppShellComponent))],
+  providers: [provideServerRendering(withRoutes(serverRoutes))],
   bootstrap: [AppComponent],
-  declarations: [
-    AppShellComponent
-  ],
 })
 export class AppServerModule {}
