06b0cf6ba5ef5065f7b96704ba60df838a07a23f
NetSinx <yasin03ckm@gmail.com>
2025-10-16 17:59:10 +0700

    adding images product

--- app/src/app/components/product/product.component.ts
+++ app/src/app/components/product/product.component.ts
@@ -12,10 +12,47 @@ import { LoadingService } from 'src/app/services/loading/loading.service';
 
 export class ProductComponent implements OnInit {
   promoProducts: Product[] = []
+  flashSaleProducts: Product[] = []
   activeSidebar: string = ""
 
   constructor(private productService: ProductService, private loadingService: LoadingService) {
     this.promoProducts = [
+      {
+        id: 1,
+        nama: "Baju Muslim Keren & Kekinian",
+        images: "assets/img/baju_muslim1.jpg",
+        slug: 'baju-muslim-keren-kekinian',
+        deskripsi: 'Baju muslim keren dan kekinian',
+        kategori_id: 1,
+        harga: 17000,
+        stok: 25,
+        rating: 5.0
+      },
+      {
+        id: 2,
+        nama: "Baju Koko Pria Beragam Ukuran S/M/L/XL/XXL",
+        images: "assets/img/baju_muslim2.jpg",
+        slug: 'baju-koko-pria-beragam-ukuran',
+        deskripsi: 'Baju koko pria beragam ukuran',
+        kategori_id: 1,
+        harga: 17000,
+        stok: 12,
+        rating: 5.0
+      },
+      {
+        id: 3,
+        nama: "Koko Pria Murah Berkualitas",
+        images: "assets/img/baju_muslim3.jpg",
+        slug: 'koko-pria-murah-berkualitas',
+        deskripsi: 'Baju koko pria murah dan berkualitas',
+        kategori_id: 1,
+        harga: 25000,
+        stok: 8,
+        rating: 5.0
+      }
+    ]
+
+    this.flashSaleProducts = [
       {
         id: 1,
         nama: "Baju Muslim Keren & Kekinian",
@@ -85,5 +122,9 @@ export class ProductComponent implements OnInit {
 
   public setActiveSidebar(sidebar: string): void {
     this.activeSidebar = sidebar
+  }
+
+  public ratingNumber(n: number): number[] {
+    return Array.from({ length: n }, (_, i) => i)
   }
 }
